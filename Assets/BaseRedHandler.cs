3b06d9408b5d9ae7410dc69cda2fbf58ed415eea
SeanRog <seanmikerog@gmail.com>
2023-06-07 20:09:21 -0400

    Got circle to spawn and move

---
+++ Assets/BaseRedHandler.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseRedHandler : MonoBehaviour
+{
+	[SerializeField] private Rigidbody2D baseRed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
